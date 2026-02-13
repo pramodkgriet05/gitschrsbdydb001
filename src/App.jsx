0818cee611b71ca01eabf38ac22238578ca25019
pramod <pramodk.griet05@gmail.com>
2026-02-13 20:12:07 +0530

    check routing010

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Home from './home/Home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Login from './login/Login';
 import School from './school/School';
 import School_1 from './school/School/School_1';
@@ -13,13 +13,18 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-         <Route path="/login" element={<Login />} />
-          <Route path="/home/school" element={<School />} />
-            <Route path="/home/school/1" element={<School_1 />} />
-              <Route path="/home/school/1/2" element={<School_1_2 />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/home/school" element={<School />} />
+      <Route path="/home/school/1" element={<School_1 />} />
+      <Route path="/home/school/1/2" element={<School_1_2 />} />
+      <Route path="/home/school/login/1" element={<Login />} />
+      
+    
         
       </Routes>
+      
+      
     </BrowserRouter>
   );
 }
--- src/school/School/School_1_2.jsx
+++ src/school/School/School_1_2.jsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom"
+
 function School_1_2()
 {
     return(
+        <div>
        <div><h1>school/2</h1></div>
+      
+       
+        
+       <Link to="/home/school/login/1">home.login.1</Link>
+       </div>
+        
+      
     )
 }
 export default School_1_2
